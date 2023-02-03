2d67a235a2970d108b40c0ccc5c7629860e046df
gabriel quesnot <gaqu1994@gmail.com>
2023-02-03 05:52:28 +0100

    fix text

--- config/queue.php
+++ config/queue.php
@@ -65,7 +65,7 @@ return [
 
         'redis' => [
             'driver' => 'redis',
-            'connection' => 'default',
+            'connection' => 'redis',
             'queue' => env('REDIS_QUEUE', 'default'),
             'retry_after' => 120,
             'block_for' => 0,
