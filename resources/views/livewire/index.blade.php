05fdcf0d836c6b9b5e9d3b1b6966299194145c49
gabriel quesnot <gaqu1994@gmail.com>
2023-02-03 07:18:00 +0100

    remove sync in home

--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -4,7 +4,7 @@
     <div class="w-1/2 mx-2 flex flex-col">
         <div class="w-1/2 flex ">
             <h3 class="text-3xl font-bold">Search Summoner</h3>
-            <button wire:click="sync" class=" ml-4 w-fit h-fit inline-flex items-center mr-4 px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Sync</button>
+{{--            <button wire:click="sync" class=" ml-4 w-fit h-fit inline-flex items-center mr-4 px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Sync</button>--}}
         </div>
         <div class="mt-1 sm:mt-0 sm:col-span-2 my-4">
             <input wire:model.lazy="summonerName" type="text" name="SummonerName" id="SummonerName"
