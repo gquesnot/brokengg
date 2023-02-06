a43c1efe4b8679b2c85d992fe9ba1da76f29c401
gabriel quesnot <gaqu1994@gmail.com>
2023-02-07 00:07:57 +0100

    fix filters

--- app/Traits/QueryParamsTrait.php
+++ app/Traits/QueryParamsTrait.php
@@ -2,12 +2,14 @@
 
 namespace App\Traits;
 
+use function PHPUnit\Framework\isEmpty;
+
 trait QueryParamsTrait
 {
-    public function getParamsUrl()
+    public function getParamsUrl(): string
     {
         $result = [];
-        if ($this->filters != null) {
+        if ($this->filters) {
             foreach ($this->filters as $key => $value) {
                 if ($value != null) {
                     $result[$key] = $value;
@@ -19,7 +21,9 @@ trait QueryParamsTrait
                 }
             }
         }
-
-        return '?'.http_build_query($result);
+        if (!isEmpty($result) > 0) {
+            return '?' . http_build_query($result);
+        }
+        return "";
     }
 }
